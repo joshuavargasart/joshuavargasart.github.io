3f80b12ae297f0c2279b385d50cd5c26378db7a1
joshuavargasart <joshua.vargas@sjsu.edu>
2025-04-09 13:14:30 -0700

    added selfie

---
+++ selfie/selfie.js
@@ -0,0 +1,15 @@
+function setup() {
+createCanvas (600,600);
+background (250);
+}
+
+function draw() { 
+  background(204); 
+  ellipse(300, 300, 350, 400);
+point(200, 80);
+arc(290, 60, 80, 80, PI, TWO_PI+HALF_PI);
+rect(180, 60, 220, 40);
+rect(250, 300, 80, 30);
+ellipse(250, 220, 50, 50);
+ellipse(357,225,50,50);
+}
